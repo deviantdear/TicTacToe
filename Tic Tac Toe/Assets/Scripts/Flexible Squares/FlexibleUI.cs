934ee63a131b953941c748a429f5942d045e3dcd
Valarie Adams <deviantdear@gmail.com>
2018-12-29 16:43:05 -0700

    added folders are re-imported art assets

---
+++ Tic Tac Toe/Assets/Scripts/Flexible Squares/FlexibleUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class FlexibleUI : MonoBehaviour {
+
+    public FlexibleUIData skinData;
+
+    //dictates how element is skinned
+    protected virtual void OnSkinUI()
+    {
+
+    }
+
+    public virtual void Awake()
+    {
+
+    }
+
+    public virtual void Update()
+    {
+        if (Application.isEditor)
+        {
+            OnSkinUI();
+        }
+    }
+
+}
---
+++ Tic Tac Toe/Assets/Scripts/Flexible Squares/FlexibleUIData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Contains behaviour and visual data for the properties of each square
+//Allows you to easily swap themes for different Tic tac Toe boards
+//
+
+[CreateAssetMenu(menuName = "Flexible UI Data")]
+public class FlexibleUIData : ScriptableObject
+{
+
+
+}
